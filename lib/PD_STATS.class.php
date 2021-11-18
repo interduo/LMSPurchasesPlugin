40ffe86b66433a508afd1d416dda66037c280d66
GC <gcichowski@gmail.com>
2021-11-18 21:39:31 +0100

    dashboard - dodalem definicje w funkcji statystyk

--- lib/PD_STATS.class.php
+++ lib/PD_STATS.class.php
@@ -4,6 +4,7 @@
 
 class PD_STATS
 {
+
 private $db;
 //    private $options = array();
 
@@ -14,15 +15,59 @@ private $db;
 
     public function PDStats()
     {
-        global $PDSTATS;
-        $sql = '';
-            $sql .= ' COUNT(*) AS overdue,';
+
+// pdstats definitions - todo: wynieść ponad funkcję, klasę, może nawet w osobny plik i korzystać równeiż w filtrach
+define('PD_PAYED', 0);
+define('PD_OVERDUE', 1);
+define('PD_TODAY', 2);
+define('PD_IN3DAYS', 3);
+define('PD_IN7DAYS', 4);
+define('PD_IN14DAYS', 5);
+
+$PDSTATS = array(
+    PD_PAYED => array(
+        'summarylabel' => trans('Payed:'),
+        'filter' => 'paydate IS NOT NULL',
+        'alias' => 'payed'
+    ),
+    PD_OVERDUE => array(
+        'summarylabel' => trans('Today:'),
+        'filter' => 'paydate IS NULL AND deadline = ?NOW?',
+        'alias' => 'today'
+    ),
+    PD_TODAY => array(
+        'summarylabel' => trans('Overdue:'),
+        'filter' => 'paydate IS NULL AND deadline+86399 < ?NOW?',
+        'alias' => 'overdue'
+    ),
+    PD_IN3DAYS => array(
+        'summarylabel' => trans('In 3 days:'),
+        'filter' => 'paydate IS NULL AND deadline - ?NOW? < 3*86400',
+        'alias' => 'in3days'
+    ),
+    PD_IN7DAYS => array(
+        'summarylabel' => trans('In 7 days:'),
+        'filter' => 'paydate IS NULL AND deadline - ?NOW? < 7*86400',
+        'alias' => 'in7days'
+    ),
+    PD_IN14DAYS => array(
+        'summarylabel' => trans('In 14 days:'),
+        'filter' => 'paydate IS NULL AND deadline - ?NOW? < 14*86400',
+        'alias' => 'in14days'
+    ),
+);
+
+//        global $PDSTATS;
+       $sql = '';
+        foreach ($PDSTATS as $statusidx => $status) {
+            $sql .= ' COUNT(CASE WHEN ' . $status['filter'] . ' THEN 1 END) AS ' . $status['alias'] . ',
+            SUM(CASE WHEN ' . $status['filter'] . ' THEN grossvalue END) AS '.$status['alias'].'value,
+            ';
+        }
         $result = $this->db->GetRow(
             'SELECT
-            SUM(grossvalue) AS overduevalue,
-            ' . $sql . '  COUNT(id) AS total
+            ' . $sql . ' COUNT(id) AS unpaid
             FROM pds
-            WHERE paydate IS NULL AND (deadline+86399 < ?NOW?)
             '
         );
         return $result;
