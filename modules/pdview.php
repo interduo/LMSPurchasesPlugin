af5c465400bbcba500f8805b83b35a6bb94b10cb
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2021-11-30 11:43:33 +0100

    podgląd faktury po kliku w wiersz

--- modules/pdview.php
+++ modules/pdview.php
@@ -5,6 +5,8 @@ $id = intval($_GET['id']);
 $PURCHASES = LMSPurchasesPlugin::getPurchasesInstance();
 $firstfile = array_shift(array_values($PURCHASES->GetPurchaseDocumentFiles($id)));
 
-$SESSION->redirect($firstfile['fullpath']);
+if (!empty($firstfile)) {
+    $SESSION->redirect($firstfile['fullpath']);
+}
 
 ?>
