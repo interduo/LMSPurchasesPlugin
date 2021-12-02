d439dc3ff4ceb404ad7511bdb26d4259780545aa
Interduo <jkl@interduo.pl>
2021-12-02 09:27:01 +0100

    Update uninstall.sql

--- doc/uninstall.sql
+++ doc/uninstall.sql
@@ -5,10 +5,13 @@ DROP SEQUENCE IF EXISTS pdcategories_id_seq;
 DROP TABLE IF EXISTS pdcategories CASCADE;
 DROP SEQUENCE IF EXISTS pds_id_seq;
 DROP TABLE IF EXISTS pds CASCADE;
+DROP SEQUENCE IF EXISTS pddoccat_id_seq;
+DROP TABLE IF EXISTS pddoccat CASCADE;
 DROP SEQUENCE IF EXISTS pdprojects_id_seq;
 DROP TABLE IF EXISTS pdprojects CASCADE;
 DROP SEQUENCE IF EXISTS pdattachments_id_seq;
 DROP TABLE IF EXISTS pdattachments CASCADE;
+
 DELETE FROM dbinfo WHERE keytype='dbversion_LMSPurchasesPlugin';
 DELETE FROM uiconfig WHERE section='pd';
 DELETE FROM uiconfig WHERE section='phpui' AND var='plugins';
