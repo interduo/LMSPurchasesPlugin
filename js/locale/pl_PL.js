dae2fd4aff300a9de07bf84ecf2825de8a5514ed
Rafał Wójcik <rw@awbnet.pl>
2021-12-27 20:25:56 +0100

    bugfix: corrected typos and grammar

--- js/locale/pl_PL.js
+++ js/locale/pl_PL.js
@@ -1,3 +1,3 @@
 ﻿
 $_LANG['Are you sure you want to delete that purchase document?'] = 'Czy jesteś pewien, że chcesz skasować ten dokument kosztowy?';
-$_LANG['Are you sure you want to delete that purchase category?'] = 'Czy jesteś pewien, że chcesz skasować tą kategorię wydatku?';
+$_LANG['Are you sure you want to delete that purchase category?'] = 'Czy jesteś pewien, że chcesz skasować tę kategorię wydatku?';
