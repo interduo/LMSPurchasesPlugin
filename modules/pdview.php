f6065ff01050a6ce88c8da5a9c6b7c432b45f8a1
Jarosław Kłopotek - INTERDUO <jkl@interduo.pl>
2022-06-20 17:08:44 +0200

    popraweczki

--- modules/pdview.php
+++ modules/pdview.php
@@ -11,23 +11,23 @@ $PURCHASES = LMSPurchasesPlugin::getPurchasesInstance();
 
 $files = $PURCHASES->GetPurchaseFiles((empty($id) ? array('attid' => $attid) : array('pdid' => $id)));
 
-if (empty($files)) {
-    die("No attachment for this purchase. Please go back.");
+if (!empty($files)) {
+    $file = array_shift(array_values($files));
 }
 
-$file = array_shift(array_values($files));
+if (empty($file) || !file_exists($fullfilepath)) {
+    die(trans("No attachment for this purchase."));
+}
 
 $fullfilepath = ConfigHelper::getConfig('pd.storage_dir', STORAGE_DIR . DIRECTORY_SEPARATOR . 'pd') . DIRECTORY_SEPARATOR . $file['filepath'] . DIRECTORY_SEPARATOR . $file['filename'];
 
-if (file_exists($fullfilepath)) {
-    $file['content'] = file_get_contents($fullfilepath);
+$file['content'] = file_get_contents($fullfilepath);
 
-    header('Content-Disposition: inline; filename=' . $file['filename']);
-    header('Content-Type: ' . $file['type']);
-    header('Content-Length: ' . filesize($fullfilepath));
-    header('Pragma: public');
-    header('Cache-Control: private, max-age=0, must-revalidate');
-    ini_set('zlib.output_compression', '0');
+header('Content-Disposition: inline; filename=' . $file['filename']);
+header('Content-Type: ' . $file['type']);
+header('Content-Length: ' . filesize($fullfilepath));
+header('Pragma: public');
+header('Cache-Control: private, max-age=0, must-revalidate');
+ini_set('zlib.output_compression', '0');
 
-    die($file['content']);
-}
+die($file['content']);
